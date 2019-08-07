2dab92ec9be3d783a39160dbd645d1f6922443ff
Taro Kashiwagi <mad.yc.oc.lp@gmail.com>
2019-08-07 21:28:17 +0900

    first commit

---
+++ src/components/Canvas.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .div
+</template>
+
+<script>
+import * as PIXI from 'pixi.js'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    this.app = new PIXI.Application({
+      transparent: true,
+      antialias: true
+    });
+    this.$el.appendChild(this.app.view);
+    this.app.renderer.view.style.display = "block";
+    this.app.renderer.autoResize = true;
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
